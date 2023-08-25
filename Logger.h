581db2dc529c788369d73aae9ccc756edc37f211
Iliy <dudnikov26@mail.ru>
2023-08-25 15:36:48 +0300

    change

--- Logger.h
+++ Logger.h
@@ -4,27 +4,40 @@
 #include <sstream>
 #include <thread>
 #include <filesystem>
+#include <mutex>
+
+static std::mutex mut;
 
 class Logger
 {
 public:
 	void operator()(std::string str)
 	{
-		if(!std::filesystem::exists("log.txt"))
-		{
-			std::ofstream f("log.txt", std::ios::trunc | std::ios::out);
-			f.close();
-		}	
 
-	std::ofstream file("log.txt", std::ios::app);
+		if(!std::filesystem::exists(_file_name))
+		{
+			std::ofstream f(_file_name, std::ios::trunc | std::ios::out);
+			f.close();
+		}
+
+	std::ofstream file(_file_name, std::ios::app);
+
+	mut.lock();
+	
 
 	if(!file.is_open())
+	{
+		mut.unlock();
 		return;
+	}
 
 	file << "Пользовательский ввод: " << str << std::endl;	
 
 	file.close();
 
-	}
+	mut.unlock();
 
+	}
+private:
+	std::string _file_name = "log.txt";
 };
